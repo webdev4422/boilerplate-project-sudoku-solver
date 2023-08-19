e000848f52d17bbe22f0dbdf4a95739dbc2ce698
user <user@MacBook-Pro-user.local>
2023-08-19 17:28:08 +0300

    Init

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,14 @@
-'use strict';
+'use strict'
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require('../controllers/sudoku-solver.js')
 
 module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+  let solver = new SudokuSolver()
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {})
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
-};
+  app.route('/api/solve').post((req, res) => {
+    console.log('OK')
+    res.end()
+  })
+}
